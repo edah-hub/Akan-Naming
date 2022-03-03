6abb98f444ed6cca252fd0dc0c5f0c94ba7c82d2
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-04 00:07:42 +0300

    adds a form

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// function navText(){
+//     document.getElementById(nav-text)=navText.toUpperCase();
+// }
+
+function navText(){
+   let akan=document.getElementById("nav-text").innerHTML.toUpperCase()
+   alert(akan);
+}
+navText()
+
+var header = document.getElementById("nav-text");
+header.innerHTML = header.innerHTML.toUpperCase();
