9cb97bf006ffa13c5899a7a41743250ca2facae0
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-04 23:14:58 +0300

    adds a function to main.js function

--- js/main.js
+++ js/main.js
@@ -50,5 +50,23 @@ function getGender () {
     }
   }
 
+  function dayValidator () {
+   if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+     if (dayOfBirth > 28 || dayOfBirth < 1) {
+       return false;
+     } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+       return false;
+     } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+       return false;
+     } else {
+       return true;
+     }
+   } else if (dayOfBirth < 1 || dayOfBirth > 31){
+     return false;
+   } else {
+     return true;
+   }
+ }
+
 
  
