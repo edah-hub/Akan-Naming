e6f3edd33392db9e701cd2ac4bb9c69601b838eb
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-04 22:22:48 +0300

    adds a function to main.js file

--- js/main.js
+++ js/main.js
@@ -13,11 +13,19 @@ form.addEventListener('submit', (event) => {
    event.preventDefault();
 });
 
-function navText(){
-   let akan=document.getElementById("nav-text").innerHTML.toUpperCase()
-   alert(akan);
-}
+// function navText(){
+//    let akan=document.getElementById("nav-text").innerHTML.toUpperCase()
+//    alert(akan);
+// }
 navText()
 
 var header = document.getElementById("nav-text");
 header.innerHTML = header.innerHTML.toUpperCase();
+
+// To Get Akan Name
+function getAkanName () {
+   let yearOfBirth = document.getElementById("year-input").value;
+   let monthOfBirth = Number(document.getElementById("month-input").value);
+   let dayOfBirth = Number(document.getElementById("day-input").value);
+}
+
