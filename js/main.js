5fc817d83ebc4ff580f914a386d72cae171d7a52
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-03-04 11:53:42 +0300

    updates form

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@
 //     document.getElementById(nav-text)=navText.toUpperCase();
 // }
 
+const form  = document.getElementById('signup');
+
+form.addEventListener('submit', (event) => {
+    // handle the form data
+});
+
+form.addEventListener('submit', (event) => {
+   // stop form submission
+   event.preventDefault();
+});
+
 function navText(){
    let akan=document.getElementById("nav-text").innerHTML.toUpperCase()
    alert(akan);
